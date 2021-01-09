bfd20ac917b2f092cfc0f03023be3a4f5a3c5ba2
Jeroen Zwart <jeroen.zwart@live.com>
2021-01-09 20:06:41 +0100

    added renderlist function

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,6 +7,19 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      </div>)
+    })
+  }
+
   render() {
     return <div>StreamList</div>;
 

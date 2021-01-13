7a3ded5b4a3dc640cf99c6ef1361196d449065c2
Jeroen Zwart <jeroen.zwart@live.com>
2021-01-13 14:57:30 +0100

    added programmatic navigation to creatstream

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,6 @@
 import streams from '../apis/streams';
+import history from '../history';
+
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -27,6 +29,7 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
